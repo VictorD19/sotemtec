9e297647263804f5b5b5089df10501fc6ba399b6
macdo <ing.macdo@gmail.com>
2024-11-15 15:46:55 -0300

    feat: ajuste html e js

--- index.js
+++ index.js
@@ -23,7 +23,53 @@ function animateOnScroll() {
         }
     });
 }
+function scroll() {
 
-// Adiciona o evento de scroll e executa uma vez no carregamento
+    var section = document.getElementById('formularioContato');
+
+    section.scrollIntoView({
+        behavior: 'smooth', 
+        block: 'start'      
+    });
+}
+function enviarFormulario(event) {
+    // Impede o comportamento padrão de envio do formulário
+    event.preventDefault();
+
+    // Captura os valores dos campos do formulário
+    const nome = document.getElementById('nome').value;
+    const celular = document.getElementById('celular').value;
+    const email = document.getElementById('email').value;
+    const projeto = document.getElementById('projeto').value;
+    const descricao = document.getElementById('descricao').value;
+
+    // Formata a mensagem para o WhatsApp
+    const mensagem = `
+      *Ola gostaria de fazer um orçamento*
+      
+      *Nome*: ${nome}
+      *Celular*: ${celular}
+      *E-mail*: ${email}
+      *Projeto*: ${projeto}
+      *Descrição*: ${descricao}
+    `;
+
+    // Codifica a mensagem para ser enviada na URL
+    const mensagemCodificada = encodeURIComponent(mensagem);
+
+    // Número do WhatsApp
+    const numeroWhatsApp = '49998218294';
+
+    // URL para abrir o WhatsApp com a mensagem preencheda
+    const urlWhatsApp = `https://wa.me/${numeroWhatsApp}?text=${mensagemCodificada}`;
+
+    // Abre a URL no navegador (fazendo o redirecionamento para o WhatsApp)
+    window.open(urlWhatsApp, '_blank');
+  }
+
+
 window.addEventListener('scroll', animateOnScroll);
 window.addEventListener('load', animateOnScroll);
+
+document.getElementById("scrollBtnFaleconoco").addEventListener('click',scroll)
+document.getElementById('scrollBtn').addEventListener('click', scroll);
